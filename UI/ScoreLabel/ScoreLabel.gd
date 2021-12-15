90e980c699a7f4ca465a7f79a10938e81f45483f
Ev01 <evpevdev@optusnet.com.au>
2021-12-15 15:07:08 +1100

    added new font for scores

--- UI/ScoreLabel/ScoreLabel.gd
+++ UI/ScoreLabel/ScoreLabel.gd
@@ -3,6 +3,7 @@ extends Label
 
 func set_player(player_node):
 	player_node.connect("got_frag", self, "_on_player_frag")
+	add_color_override("font_color", player_node.modulate)
 
 func _on_player_frag(current_score):
 	text = str(current_score)
