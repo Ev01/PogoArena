785d31a56bf69c789dbf8c755b14eb45af0b7b20
Ev01 <evpevdev@optusnet.com.au>
2021-12-15 19:14:10 +1100

    added max score setting to menu

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -6,3 +6,13 @@ onready var main = get_node("/root/Main")
 
 func _on_PlayButton_pressed():
 	main.load_new_game(player_num_button.current_value)
+
+
+
+
+func _on_Control_gui_input(event):
+	print("df")
+	if event is InputEventMouseButton:
+		if event.pressed == true:
+			grab_focus()
+			print("afsdasdf")
