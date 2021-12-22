6545ed1f80d495200eaa2392ae1ec02853d10250
nic08 <vitamin_nb@icloud.com>
2021-12-22 11:55:35 +1100

    Added functionality to quit button

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -17,7 +17,6 @@ func _on_PlayButton_pressed():
 		var player_data = player_join.get_player_data()
 		main.load_new_game(player_data)
 
-
 func _on_Control_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.pressed == true:
@@ -25,3 +24,7 @@ func _on_Control_gui_input(event):
 
 func _on_active_panel_changed():
 	play_button.disabled = not player_join.active_panel_num > 0
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
